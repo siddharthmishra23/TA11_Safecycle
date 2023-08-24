262bc7cad843213698b13ea8c28641fe23b68fca
siddharth mishra <smsiddharthmishra@gmail.com>
2023-08-25 05:13:32 +1000

    deployment setting

--- securecycle/backEnd/src/server.js
+++ securecycle/backEnd/src/server.js
@@ -2,14 +2,25 @@ import exp from "constants";
 import express from "express";
 import mysql from "mysql";
 import cors from "cors";
+import path from "path";
+import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+app.use(express.static(path.join(__dirname, "../build")));
+
 const db = mysql.createConnection({
   host: "cyclestreet.mysql.database.azure.com",
   user: "cyclestreetdb",
-  password: "Monash23",
+  password: process.env.AZURE_PASSWORD,
 });
 
 db.connect(function (err) {
@@ -17,6 +28,8 @@ db.connect(function (err) {
   console.log("Connected!");
 });
 
-app.listen(8003, () => {
-  console.log("server running on 8003");
+const PORT = process.env.PORT || 8003;
+
+app.listen(PORT, () => {
+  console.log("server running on " + PORT);
 });

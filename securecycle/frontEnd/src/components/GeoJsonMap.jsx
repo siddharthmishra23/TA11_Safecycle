e3c5127f1d26e59634935208a9f06a4a8b1be554
chinyingc <130021704+chinyingc@users.noreply.github.com>
2023-10-06 10:02:52 +1100

    try to add reset

--- securecycle/frontEnd/src/components/GeoJsonMap.jsx
+++ securecycle/frontEnd/src/components/GeoJsonMap.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
-import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup, Tooltip, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import L from 'leaflet';
 
 function MyMap() {
     const [geojsonData, setGeojsonData] = useState(null);
     const [loadingError, setLoadingError] = useState(null);
     const [showPointsFor, setShowPointsFor] = useState(null); // Add state to keep track of LineString clicked
+    const [lastClickedTrail, setLastClickedTrail] = useState(null);
+    const [clickedFeature, setClickedFeature] = useState(null);
+    const defaultPosition = [-37.8102, 144.9629]; 
+    const defaultZoom = 8;  
+    // const [mapCenter, setMapCenter] = useState(defaultPosition);
+    // const [zoom, setZoom] = useState(defaultZoom);
+    
+  const mapRef = useRef();
+    function getIconByName(name) {
+      let iconUrl;
+      
+      switch (name) {
+          case 'Car park':
+              iconUrl = './marker-parking.png';
+              break;
+          case 'Picnic table':
+              iconUrl = './marker-picnic.png';
+              break;
+          case 'Toilet':
+              iconUrl = './marker-toilet.png';
+              break;
+          case 'Water (source unreliable)':
+              iconUrl = './marker-water.png';
+              break;
+          default:
+              iconUrl = './marker-all.png'; // Optional: default icon
+              break;
+      }
+  
+      return new L.Icon({
+          iconUrl: iconUrl,
+          iconSize: [25, 41], // Size of the icon, you may need to adjust this based on your icon's dimensions
+          iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+          popupAnchor: [0, -41] // Point from which the popup should open relative to the iconAnchor
+      });
+  }
+    
+
     useEffect(() => {
       // get data from public folder
       fetch('/vic_trails.geojson')
@@ -25,20 +64,39 @@ function MyMap() {
     }, []);
 
     function onEachFeature(feature, layer) {
+      if (feature.properties) {
+        if (feature.properties.name) {
+            switch (feature.geometry.type) {
+                case 'Point':
+                    layer.setIcon(getIconByName(feature.properties.name));
+                    break;
+                // ... handle other geometry types if needed
+            }
+        }
       if (feature.properties && feature.properties.trail_name) {
-          layer.bindPopup(feature.properties.trail_name);
+        layer.bindPopup(`<div style="font-size: 18px;">${feature.properties.trail_name}</div>`, {
+          offset: L.point(-30, -25)
+      });}
+  
 
-          // On click of LineString, show the associated Points
+  
+          // On click of LineString, show the associated Points and update lastClickedTrail
           layer.on('click', function() {
-              setShowPointsFor(feature.properties.trail_name);
+              setClickedFeature(layer);
+              setLastClickedTrail(feature.properties.trail_name);
+              if (showPointsFor !== feature.properties.trail_name) {
+                  setShowPointsFor(feature.properties.trail_name);
+              }
           });
-
-          // On close of popup, hide the Points
+  
+          // On close of popup, hide the Points only if the trail_name isn't the same as clicked
           layer.on('popupclose', function() {
-              setShowPointsFor(null);
+              if (showPointsFor !== feature.properties.trail_name) {
+                  setShowPointsFor(null);
+              }
           });
       }
-    }
+  }
 
     if (loadingError) {
         return <div>Error: {loadingError}</div>;
@@ -63,9 +121,29 @@ function MyMap() {
         feature => feature.geometry.type === "Point" && 
         feature.properties.trail_name === showPointsFor
     );
+    function ZoomToFeature({ feature }) {
+      const map = useMap();
+  
+      useEffect(() => {
+          if (feature) {
+              map.fitBounds(feature.getBounds());
+          }
+      }, [feature, map]);
+  
+      return null;
+  }
+
+
+  
+
+    
 
     return (
-        <MapContainer center={[-37.8102, 144.9629]} zoom={8} style={{ width: '100%', height: '600px' }}>
+      <div>
+        <MapContainer center={defaultPosition}
+        zoom={defaultZoom}
+        // whenCreated={mapInstance => mapRef.current = mapInstance}
+         style={{ width: '100%', height: '600px' }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -73,11 +151,40 @@ function MyMap() {
             <GeoJSON data={lineStringData} onEachFeature={onEachFeature}/>
             {/* Render the Points only if they are to be shown */}
             {pointsToShow.map(point => (
-                <Marker position={[point.geometry.coordinates[1], point.geometry.coordinates[0]]}>
-                    <Popup>{point.properties.name}</Popup>
-                </Marker>
-            ))}
+              <Marker 
+                  position={[point.geometry.coordinates[1], point.geometry.coordinates[0]]}
+                  eventHandlers={{
+                      popupclose: (e) => {
+                          if (point.properties.trail_name === lastClickedTrail) {
+                              e.popup.openOn(e.target._map);
+                          }
+                      }
+                  }}>
+                  {/* <Popup offset={[50, -50]}>
+                  <div style={{ fontSize: '50px' }}>
+                    {point.properties.trail_name}<br />{point.properties.name}
+                  </div>
+                  </Popup> */}
+                  <Tooltip>
+                    {point.properties.name}
+                  </Tooltip>
+                  {clickedFeature && <ZoomToFeature feature={clickedFeature} />}
+              </Marker>
+))}
+
         </MapContainer>
+
+        {/* <button onClick={() => {
+          setMapCenter(defaultPosition);
+          setZoom(defaultZoom);
+          // setClickedFeature(null);
+          if (mapRef.current) {
+            mapRef.current.setView(defaultPosition, defaultZoom);
+          }
+        }}>
+          Reset
+        </button> */}
+        </div>
     );
 }
 
